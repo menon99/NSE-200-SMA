d9be71d0f3465b414254066df0f8d5eb9c1d9a8d
Akash <akashmenon11699cool@hotmail.com>
2020-12-18 19:47:40 +0530

    production ready

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const writeRecordToAvgCSV = require("./utils/csvUtils").writeRecordToAvgCsv;
 
 const main = async () => {
   let stocksList = await getStocksList();
-  stocksList = stocksList.slice(0, 2);
   const promises = stocksList.map(({ STOCK }) => addStock(STOCK));
   const records = (await Promise.all(promises)).filter(
     (ele) => Object.keys(ele).length > 0
